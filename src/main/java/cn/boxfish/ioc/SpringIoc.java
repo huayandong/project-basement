74fd6651f05078c4ef1c4fd2841d323c2baf48f0
huayandong <huayandong@jzp-iMAC.local>
2017-07-11 15:38:39 +0800

    xml方式Ioc;

--- src/main/java/cn/boxfish/ioc/SpringIoc.java
+++ src/main/java/cn/boxfish/ioc/SpringIoc.java
@@ -1,21 +1,21 @@
 package cn.boxfish.ioc;
 
 import cn.boxfish.ioc.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by huayandong on 17/6/26.
  */
 public class SpringIoc {
 
-    @Autowired
-    @Qualifier(value = "user")
-    private User user;
-
 
     public static void main(String[] args) {
 
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        User user = (User) context.getBean("user");
+
+        user.add();
     }
 
 
--- src/main/java/cn/boxfish/ioc/domain/User.java
+++ src/main/java/cn/boxfish/ioc/domain/User.java
@@ -6,4 +6,19 @@ package cn.boxfish.ioc.domain;
  */
 public class User {
 
+    private String username;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private Integer age;
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void add() {
+        System.out.println("name is: " + username + ",age is: " + age);
+    }
 }
