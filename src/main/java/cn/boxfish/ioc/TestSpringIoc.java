b69c104b29e7203732d2081508b05be12bcf81bd
huayandong <huayandong@jzp-iMAC.local>
2017-07-11 16:21:04 +0800

    注入set和list；

--- src/main/java/cn/boxfish/ioc/TestSpringIoc.java
+++ src/main/java/cn/boxfish/ioc/TestSpringIoc.java
@@ -1,21 +1,21 @@
 package cn.boxfish.ioc;
 
-import cn.boxfish.ioc.domain.User;
+import cn.boxfish.ioc.act.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by huayandong on 17/6/26.
  */
-public class SpringIoc {
+public class TestSpringIoc {
 
 
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
-        User user = (User) context.getBean("user");
+        UserService userService = (UserService) context.getBean("user");
 
-        user.add();
+        userService.add();
     }
 
 
