9b2a523a020d1f3d1d61938dc28139bc6e036a62
huayandong <huayandong@jzp-iMAC.local>
2017-07-30 11:39:07 +0800

    StringUtils.equals();

--- base/src/main/java/cn/taike/Collect.java
+++ base/src/main/java/cn/taike/Collect.java
@@ -4,9 +4,10 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by huayandong on 17/7/29.
@@ -18,13 +19,15 @@ public class Collect {
      * 底层采用的任然是 Collection.isEmpty() 和 Map.isEmpty()对集合的判断
      * 只是在判断之前做了一次封装：判断该集合对象(list/set/map)是否等于“null”
      * 如果等于“null”则直接返回true,避免出现NullPointException;
-     *  ArrayList<Long> obj = null;
-     *  obj.isEmpty();   //此时会报异常
+     * ArrayList<Long> obj = null;
+     * obj.isEmpty();   //此时会报异常
      */
-    public static void main(String[] args) {
-        ArrayList<Long> obj = null;
-        System.out.println("?" + obj.isEmpty());
-        obj.add(null);
+    public static void SampleIsEmpty() {
+        List<Double> obj = Lists.newArrayList();
+
+//        ArrayList<Long> obj = null;
+//        System.out.println("?" + obj.isEmpty());
+//        obj.add(null);
         boolean isEmpty = CollectionUtils.isEmpty(obj);
         boolean notEmpty = CollectionUtils.isNotEmpty(obj);
         boolean empty = obj.isEmpty();
@@ -41,5 +44,28 @@ public class Collect {
         map.put(null, null);
         boolean mapIsEmpty = MapUtils.isEmpty(map);
         System.out.println("Map的isEmpty判断：" + mapIsEmpty);
+
+       /*
+        * StringUtils.equals(null, null)   = true
+        * StringUtils.equals(null, "abc")  = false
+        * StringUtils.equals("abc", null)  = false
+        * StringUtils.equals("abc", "abc") = true
+        * StringUtils.equals("abc", "ABC") = false
+        */
+        boolean equals = StringUtils.equals("abc", "aBc");  //false
+        System.out.println("StringUtils: " + equals);
+    }
+
+    public static void SampleStream() {
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        SampleIsEmpty();
+
+
     }
 }
