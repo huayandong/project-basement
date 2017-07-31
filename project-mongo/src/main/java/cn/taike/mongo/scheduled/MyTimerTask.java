c2689d05751ef2a9d28fbe0e05b8d7acef0b1432
huayandong <huayandong@jzp-iMAC.local>
2017-07-31 11:49:08 +0800

    jdbcTemplate的增删改操作；

--- project-mongo/src/main/java/cn/taike/mongo/scheduled/MyTimerTask.java
+++ project-mongo/src/main/java/cn/taike/mongo/scheduled/MyTimerTask.java
@@ -14,25 +14,25 @@ public class MyTimerTask {
 
     SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 3000)  //每隔3秒执行一次
-    public void timeRate() {
-        System.out.println("执行时间: " + sdf.format(new Date()));
-    }
-
-    /**
-     * 定时执行任务参数：
-     *  @SCheduled(cron = "")
-     *   第一位，表示秒，取值0-59
-     *   第二位，表示分钟，表示0-59
-     *   第三位，表示小时，取值0-23
-     *   第四位，表示日期 天/日，取值1-31
-     *   第五位，表示日期 月份，取值1-12
-     *   第六位，表示星期，取值1-7，另注：1表示星期天，2表示星期一，...
-     *   第七位，表示年份，可以留空，取值1970-2099
-     */
-    @Scheduled(cron = "0/10 06 11 * * ?")
-    public void statusTask() {
-        System.out.println("定时执行任务: " + sdf.format(new Date()));
-    }
+//    @Scheduled(fixedRate = 3000)  //每隔3秒执行一次
+//    public void timeRate() {
+//        System.out.println("执行时间: " + sdf.format(new Date()));
+//    }
+//
+//    /**
+//     * 定时执行任务参数：
+//     *  @SCheduled(cron = "")
+//     *   第一位，表示秒，取值0-59
+//     *   第二位，表示分钟，表示0-59
+//     *   第三位，表示小时，取值0-23
+//     *   第四位，表示日期 天/日，取值1-31
+//     *   第五位，表示日期 月份，取值1-12
+//     *   第六位，表示星期，取值1-7，另注：1表示星期天，2表示星期一，...
+//     *   第七位，表示年份，可以留空，取值1970-2099
+//     */
+//    @Scheduled(cron = "0/10 06 11 * * ?")
+//    public void statusTask() {
+//        System.out.println("定时执行任务: " + sdf.format(new Date()));
+//    }
 
 }
