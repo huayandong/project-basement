ba11ee146761a891c511537ed2f3aaefc31f0648
huayandong <huayandong@jzp-iMAC.local>
2017-08-04 16:38:28 +0800

    关流的工具类；

---
+++ MavenDemo/src/main/java/cn/taike/dao/CloseUtils.java
@@ -0,0 +1,36 @@
+package cn.taike.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by huayandong on 17/8/4.
+ */
+public class CloseUtils {
+
+    public static void close(ResultSet rs, PreparedStatement pst, Connection conn) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (pst != null) {
+            try {
+                pst.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
