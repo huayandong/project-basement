4f892c2cbf1dd46700b65cc81d13559b6f885760
huayandong <huayandong@jzp-iMAC.local>
2017-08-07 15:09:27 +0800

    责任链；

---
+++ MavenDemo/src/main/java/cn/taike/responsibility/Handler.java
@@ -0,0 +1,34 @@
+package cn.taike.responsibility;
+
+/**
+ * Created by huayandong on 17/8/7.
+ */
+//责任链模式：抽象处理类
+abstract class Handler {
+
+    private Handler nextHandler;
+
+    public Handler getNextHandler() {
+        return nextHandler;
+    }
+
+    public void setNextHandler(Handler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    public abstract void doHandler();
+}
+
+class concreteHandler extends Handler {
+
+    @Override
+    public void doHandler() {
+
+        if(getNextHandler() != null){
+            System.out.println("还有责任链");
+            getNextHandler().doHandler();
+        } else {
+            System.out.println("自己处理");
+        }
+    }
+}
