ae8a470e4249b6da85390434ebf9a840f1786d37
huayandong <huayandong@jzp-iMAC.local>
2017-08-17 14:35:03 +0800

    controller异常；

---
+++ project-mongo/src/main/java/cn/taike/mongo/basement/exception/ExceptionHandlerController.java
@@ -0,0 +1,20 @@
+package cn.taike.mongo.basement.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Created by huayandong on 17/8/17.
+ */
+@Slf4j
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(Exception.class)
+    public Object allException(Exception e) {
+        log.error("all Exception.", e);
+        return ResponseEntity.badRequest().build();
+    }
+}
